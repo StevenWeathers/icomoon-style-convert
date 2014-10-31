6ac34cc282f442ca31c652e563f75502c056e6a0
Steven Weathers <steven@weathers.me>
2014-10-30 22:34:52 -0400

    Add test cases and abstract into node module useable in node scripts

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-var fs = require('fs');
-var Q = require('q');
 var _ = require('lodash');
 
+// @todo add error catching for cases where prefix doesn't match css
+var cssToLess = function(css, options){
+    if (!css) {
+        throw new Error("Need to pass css");
+    }
+    // icomoon settings
+    var classPrefix = (options.classPrefix) ? options.classPrefix : "icon-";
+    // @todo add postfix option
 
-var icoCssFile = "test/style.css";
+    // find the font-face
+    var fontFaceExpression = "(@font-face\\s\\{(\\s*?.*?)*?\\})";
+    var fontFaceRegex = new RegExp(fontFaceExpression, "igm");
+    var fontFace = css.match(fontFaceRegex);
+    // find the font-family
+    var fontFamily = fontFace[0].match(/(\'.*\')/igm)[0];
+    // find the font-buster
+    var fontBuster = fontFace[0].match(/(\?(.*)\')/igm)[0].replace("?","").replace("'","");
+    // build the font-face less
+    var fontFaceLess = "@font-face {\n\tfont-family: '@{"+classPrefix+"font-family}';\n\tsrc:url('fonts/@{"+classPrefix+"font-family}.eot?@{"+classPrefix+"font-buster}');\n\tsrc:url('fonts/@{"+classPrefix+"font-family}.eot?#iefix@{"+classPrefix+"font-buster}') format('embedded-opentype'),\n\t\turl('fonts/@{"+classPrefix+"font-family}.woff?@{"+classPrefix+"font-buster}') format('woff'),\n\t\turl('fonts/@{"+classPrefix+"font-family}.ttf?@{"+classPrefix+"font-buster}') format('truetype'),\n\t\turl('fonts/@{"+classPrefix+"font-family}.svg?@{"+classPrefix+"font-buster}#@{"+classPrefix+"font-family}') format('svg');\n\tfont-weight: normal;\n\tfont-style: normal;\n}";
+    // build the font icon styles
+    var fontIconStyles = "." + classPrefix + "font-styles(){\n\tfont-family: '@{"+classPrefix+"font-family}';\n\tspeak: none;\n\tfont-style: normal;\n\tfont-weight: normal;\n\tfont-variant: normal;\n\ttext-transform: none;\n\tline-height: 1;\n\n\t// Better Font Rendering\n\t-webkit-font-smoothing: antialiased;\n\t-moz-osx-font-smoothing: grayscale;\n}";
+    // build the font icon selector styles
+    var fontIconSelector = "[class^=\"" + classPrefix + "\"], [class*=\" " + classPrefix + "\"] {\n\t." + classPrefix + "font-styles();\n}";
 
-// icomoon settings
-var classPrefix = "icon-";
-var classPostFix = "";
-// find the icons
-var iconsExpression = "(\\."+classPrefix+".*"+classPostFix+"\\s\\{[\\s\\S].*[\\s\\S]\\})+";
-var iconsRegex = new RegExp(iconsExpression, "igm");
-// find the icon name
-var singleIconExpression = "("+classPrefix+".*(?=\\:))+";
-var singleIconRegex = new RegExp(singleIconExpression,"ig");
-// find the icon content
-var iconContentExpression = "(\".*\")+";
-var iconContentRegex = new RegExp(iconContentExpression,"ig");
-// find the font-face
-var fontFaceExpression = "(@font-face\\s\\{(\\s*?.*?)*?\\})";
-var fontFaceRegex = new RegExp(fontFaceExpression, "igm");
+    // find the icons
+    var iconsExpression = "(\\."+classPrefix+".*\\s\\{[\\s\\S].*[\\s\\S]\\})+";
+    var iconsRegex = new RegExp(iconsExpression, "igm");
+    var icons = css.match(iconsRegex);
+    // find the icon name
+    var singleIconExpression = "("+classPrefix+".*(?=\\:))+";
+    var singleIconRegex = new RegExp(singleIconExpression,"ig");
+    // find the icon content
+    var iconContentExpression = "(\".*\")+";
+    var iconContentRegex = new RegExp(iconContentExpression,"ig");
 
-var buildLessFile = function(css){
     var iconsLess = "";
     var iconFontVariables = [];
     var iconsVariablesLess = [];
     var iconsClassLess = [];
-    var icons = css.match(iconsRegex);
-    var fontFace = css.match(fontFaceRegex);
-    var fontFamily = fontFace[0].match(/(\'.*\')/igm)[0];
-    var fontBuster = fontFace[0].match(/(\?(.*)\')/igm)[0].replace("?","").replace("'","");
-    var fontFaceLess = "@font-face {\n\tfont-family: '@{"+classPrefix+"font-family}';\n\tsrc:url('fonts/@{"+classPrefix+"font-family}.eot?@{"+classPrefix+"font-buster}');\n\tsrc:url('fonts/@{"+classPrefix+"font-family}.eot?#iefix@{"+classPrefix+"font-buster}') format('embedded-opentype'),\n\t\turl('fonts/@{"+classPrefix+"font-family}.woff?@{"+classPrefix+"font-buster}') format('woff'),\n\t\turl('fonts/@{"+classPrefix+"font-family}.ttf?@{"+classPrefix+"font-buster}') format('truetype'),\n\t\turl('fonts/@{"+classPrefix+"font-family}.svg?@{"+classPrefix+"font-buster}#@{"+classPrefix+"font-family}') format('svg');\n\tfont-weight: normal;\n\tfont-style: normal;\n}";
-    var fontIconStyles = "." + classPrefix + "font-styles(){\n\tfont-family: '@{"+classPrefix+"font-family}';\n\tspeak: none;\n\tfont-style: normal;\n\tfont-weight: normal;\n\tfont-variant: normal;\n\ttext-transform: none;\n\tline-height: 1;\n\n\t// Better Font Rendering\n\t-webkit-font-smoothing: antialiased;\n\t-moz-osx-font-smoothing: grayscale;\n}";
-    var fontIconSelector = "[class^=\"" + classPrefix + "\"], [class*=\" " + classPrefix + "\"] {\n\t." + classPrefix + "font-styles();\n}";
 
     iconFontVariables.push("@" + classPrefix + "font-family: " + fontFamily + ";");
     iconFontVariables.push("@" + classPrefix + "font-buster: " + fontBuster + ";");
@@ -50,15 +56,14 @@ var buildLessFile = function(css){
     return iconsLess;
 };
 
-Q.nfcall(fs.readFile, icoCssFile, "utf-8")
-.then(buildLessFile)
-.then(function(iconsLess){
-    return Q.nfapply(fs.writeFile, ["test/icons.less", iconsLess]);
-})
-.catch(function (error) {
-    // Handle any error from all above steps
-    console.log(error);
-})
-.done(function(){
-    console.log("icon-variables.less Created.");
-});
+// @todo add Stylus support
+// @todo add SCSS support
+module.exports = {
+    less: function(css, options){
+        if (!options){
+            var options = {};
+        }
+        var less = cssToLess(css, options);
+        return less;
+    }
+}
--- test/test.js
+++ test/test.js
@@ -1,10 +1,32 @@
 var assert = require("assert");
+var fs = require('fs');
+var icomoonconvert = require("../index.js");
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
+var icoCss = fs.readFileSync("test/style.css","utf-8");
+var icoLess = fs.readFileSync("test/style.less","utf-8");
+
+var icoPrefixedCss = fs.readFileSync("test/prefixed-style.css","utf-8");
+var icoPrefixedLess = fs.readFileSync("test/prefixed-style.less","utf-8");
+
+describe('icomoon-style-convert', function(){
+	describe('passing css', function(){
+		it('should be converted to less', function(){
+			assert.equal(icoLess, icomoonconvert.less(icoCss));
+		});
+	});
+
+	describe('passing no css', function(){
+		it('should throw "Need to pass css" error', function(){
+			assert.throws(icomoonconvert.less, Error, "Need to pass css");
+		});
+	});
+
+	describe('passing css with prefix option', function(){
+		it('should be converted to less', function(){
+			var options = {
+				classPrefix: "sw-"
+			};
+			assert.equal(icoPrefixedLess, icomoonconvert.less(icoPrefixedCss, options));
+		});
+	});
 });
