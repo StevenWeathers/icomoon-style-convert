b510323bd140b2f6e73343b2f76d1a79ff84cab4
Steven Weathers <steven.m.weathers@lowes.com>
2014-10-31 09:10:13 -0400

    Abstract less file building to a swig template

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var _ = require('lodash');
+var swig = require('swig');
+swig.setDefaults({ varControls: ['<%=', '%>'] });
 
 var variablesFromCss = function(css, options){
     var abstractedVariables = {};
 
     // icomoon settings
     var classPrefix = (options.classPrefix) ? options.classPrefix : "icon-";
+    abstractedVariables.classPrefix = classPrefix;
     // @todo add postfix option
 
     // find the font-face
@@ -12,7 +15,7 @@ var variablesFromCss = function(css, options){
     var fontFaceRegex = new RegExp(fontFaceExpression, "igm");
     var fontFace = css.match(fontFaceRegex);
     // find the font-family
-    abstractedVariables.fontFamily = fontFace[0].match(/(\'.*\')/igm)[0];
+    abstractedVariables.fontFamily = fontFace[0].match(/(\'.*\')/igm)[0].replace(/\'/g, "\"");
     // find the font-buster
     abstractedVariables.fontBuster = fontFace[0].match(/(\?(.*)\')/igm)[0].replace("?","").replace("'","");
 
@@ -46,35 +49,9 @@ var cssToLess = function(css, options){
         throw new Error("Need to pass css");
     }
 
-    // icomoon settings
-    var classPrefix = (options.classPrefix) ? options.classPrefix : "icon-";
-    // @todo add postfix option
-    
     var abstractedVariables = variablesFromCss(css, options);
-    
-    // build the font-face less
-    var fontFaceLess = "@font-face {\n\tfont-family: '@{"+classPrefix+"font-family}';\n\tsrc:url('fonts/@{"+classPrefix+"font-family}.eot?@{"+classPrefix+"font-buster}');\n\tsrc:url('fonts/@{"+classPrefix+"font-family}.eot?#iefix@{"+classPrefix+"font-buster}') format('embedded-opentype'),\n\t\turl('fonts/@{"+classPrefix+"font-family}.woff?@{"+classPrefix+"font-buster}') format('woff'),\n\t\turl('fonts/@{"+classPrefix+"font-family}.ttf?@{"+classPrefix+"font-buster}') format('truetype'),\n\t\turl('fonts/@{"+classPrefix+"font-family}.svg?@{"+classPrefix+"font-buster}#@{"+classPrefix+"font-family}') format('svg');\n\tfont-weight: normal;\n\tfont-style: normal;\n}";
-    // build the font icon styles
-    var fontIconStyles = "." + classPrefix + "font-styles(){\n\tfont-family: '@{"+classPrefix+"font-family}';\n\tspeak: none;\n\tfont-style: normal;\n\tfont-weight: normal;\n\tfont-variant: normal;\n\ttext-transform: none;\n\tline-height: 1;\n\n\t// Better Font Rendering\n\t-webkit-font-smoothing: antialiased;\n\t-moz-osx-font-smoothing: grayscale;\n}";
-    // build the font icon selector styles
-    var fontIconSelector = "[class^=\"" + classPrefix + "\"], [class*=\" " + classPrefix + "\"] {\n\t." + classPrefix + "font-styles();\n}";
 
-    var iconsLess = "";
-    var iconFontVariables = [];
-    var iconsVariablesLess = [];
-    var iconsClassLess = [];
-
-    iconFontVariables.push("@" + classPrefix + "font-family: " + abstractedVariables.fontFamily + ";");
-    iconFontVariables.push("@" + classPrefix + "font-buster: " + abstractedVariables.fontBuster + ";");
-
-    _(abstractedVariables.icons).forEach(function(icon) {
-        iconsVariablesLess.push("@" + icon.iconName + ": " + icon.iconContent + ";");
-        iconsClassLess.push("." + icon.iconName + ":before {\n\tcontent: @" + icon.iconName + ";\n}");
-    });
-
-    iconsLess = "// file generated do not manually edit\n\n" + iconFontVariables.join("\n") + "\n\n" + iconsVariablesLess.join("\n") + "\n\n" + fontFaceLess + "\n\n" + fontIconStyles + "\n\n" + fontIconSelector + "\n\n" + iconsClassLess.join("\n");
-
-    return iconsLess;
+    return swig.renderFile('templates/less.html', abstractedVariables);
 };
 
 // @todo add Stylus support
